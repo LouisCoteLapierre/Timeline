b90afcd46541e13c4f6ac0770c820be63da223b5
Louis Cote-Lapierre <lcotelapierre@gmail.com>
2020-08-02 18:28:46 -0400

    MainMenuWindow
    Creation of the layout of the main menu window, fixing some ignores

---
+++ TimelineDataExporter/Windows/MainMenu.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows;
+
+namespace TimelineDataExporter
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainMenu : Window
+    {
+        public MainMenu()
+        {
+            InitializeComponent();
+        }
+    }
+}
